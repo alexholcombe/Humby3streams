08fe292ab2792dd64ce329fb59ea996a053d59e1
Alex O. Holcombe <aoholcombe@gmail.com>
2018-05-17 15:36:24 +1000

    minimal reproducible error program

--- tests/lowerPsychopyBug.py
+++ tests/lowerPsychopyBug.py
@@ -1,18 +1,8 @@
 import psychopy.info
-from psychopy import monitors, visual, event, data, logging, core, sound, gui
-monitorname = 'testmonitor'
-waitBlank = False
-mon = monitors.Monitor(monitorname,width=30, distance=57)#relying on  monitorwidth cm (39 for Mitsubishi to do deg calculations) and gamma info in calibratn
-mon.setSizePix( (1024,768) )
-units='deg' #'cm'
-bgColor = [-.7,-.7,-.7] # [-1,-1,-1]
-allowGUI = False
-waitBlank = True
-scrn = 0
-fullscr=True
+from psychopy import visual
+
 def openMyStimWindow(): #make it a function because have to do it several times, want to be sure is identical each time
-    myWin = visual.Window(monitor=mon,size=(1024,768),allowGUI=allowGUI,units=units,color=bgColor,colorSpace='rgb',fullscr=fullscr,screen=scrn,waitBlanking=waitBlank,
-                   winType='pyglet' ) #pygame doesn't work, don't know why. Works in textLocationTest.py
+    myWin = visual.Window( ) #pygame doesn't work, don't know why. Works in textLocationTest.py
     return myWin
 myWin = openMyStimWindow()
 
