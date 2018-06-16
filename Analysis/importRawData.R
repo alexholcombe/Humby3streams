c846b2ac1e54d4cc36da8d1dfc463248e2a212ee
Alex O. Holcombe <aoholcombe@gmail.com>
2018-06-16 17:25:13 +1000

    analysing pilot data

--- Analysis/importRawData.R
+++ Analysis/importRawData.R
@@ -1,15 +1,41 @@
 rawDataPath<- "../rawData"
-dataFileName<- "auto_14Jun2018_14-31.txt"
+dataFileName<- "Hubert_14Jun2018_15-24_150soams.txt" # "auto_14Jun2018_15-04.txt"
 fileWithPath<- file.path(rawDataPath,dataFileName)
 
 df <-read.table(fileWithPath,sep="\t", header=TRUE)
 files <- dir(path=rawDataPath,pattern='.txt')  #find all data files in this directory
 
-df$pCorrAll <- (df$corrResp0 + df$corrResp1 + df$corrResp2) / 3
-
 library(dplyr)
-df <- df %>% mutate( numTargets =   (resp2 =="-999 ") ) #True if 3 targets, False if 2 targets
+df <- df %>% mutate( numTargets =   (resp2 !="-999 ") ) #True if 3 targets, False if 2 targets
 df$numTargets <- as.numeric(df$numTargets) + 2
+
+#Calculate corrStream0, corrStream1, corrStream2
+df<-df %>% mutate(corrStrm0 = case_when( whichStreamResp0==0 ~ corrResp0,
+                                         whichStreamResp1==0 ~ corrResp1,
+                                         whichStreamResp2==0 ~ corrResp2,
+                                         TRUE ~ NA_real_ ) )
+df<-df %>% mutate(corrStrm1 = case_when( whichStreamResp0==1 ~ corrResp0,
+                                         whichStreamResp1==1 ~ corrResp1,
+                                         whichStreamResp2==1 ~ corrResp2,
+                                         TRUE ~ NA_real_ ) )
+df<-df %>% mutate(corrStrm2 = case_when( whichStreamResp0==2 ~ corrResp0,
+                                         whichStreamResp1==2 ~ corrResp1,
+                                         whichStreamResp2==2 ~ corrResp2,
+                                         TRUE ~ NA_real_ ) )
+table(df$corrStrm0,df$numTargets)
+#table(df$whichStreamResp0,df$corrResp0)
+#table(df$whichStreamResp0,df$corrResp1)
+#table(df$whichStreamResp2,df$corrResp2)
+table(df$corrStrm1,df$numTargets)
+
+df<-df %>% mutate(pCorrAll = case_when(numTargets == 2 ~ (corrResp0 + corrResp1) / 2,
+                                       numTargets == 3 ~ (corrResp0+corrResp1+corrResp2) / 3,
+                                       TRUE ~ -900))
+table(df$numTargets,df$pCorrAll)
+
+df %>% group_by(numTargets) %>% summarise_at(vars(corrStrm0:corrStrm2), mean, na.rm=TRUE)
+
+
 
 dfAll<-data.frame()
 for (i in 1:length(files)) { #read in each file
